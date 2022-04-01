014a3e418ed8f4ccf610750996c5b6fd904c1d2a
Mac <hung.mac@dbschenker.com>
2022-03-31 23:57:10 -0400

    add user registration, profile, app guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegistrationComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
