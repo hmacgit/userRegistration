dbb944f5b5f0d9daadfa2eb6329849b4d344a325
Mac <hung.mac@dbschenker.com>
2022-04-01 00:03:09 -0400

    implement routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AuthGuard} from './auth.guard';
+import {UserProfileComponent} from './user-profile/user-profile.component';
+import {UserRegistrationComponent} from './user-registration/user-registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'login',
+    component: UserRegistrationComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'profile',
+    component: UserProfileComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login'
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
