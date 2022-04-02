7ea20b3ff20fa8f675bd34cbb76177a13da6eb6b
Mac <hung.mac@dbschenker.com>
2022-04-01 22:08:31 -0400

    service setup end points

---
+++ src/store/auth/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {AuthenticationStateModel} from './auth.state';
+
+const getAuthEP = 'https://mocki.io/v1/7f434df6-a4ac-4817-ab7c-dd39a564d01d'
+const getProfileEP = 'https://mocki.io/v1/611a3036-4420-48a5-b8da-9b461853cdd2'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private _http: HttpClient) {
+  }
+
+  getAuth() {
+    return this._http.get<boolean>(getAuthEP);
+  }
+
+  getProfile() {
+    return this._http.get<AuthenticationStateModel>(getProfileEP);
+  }
+
+}
