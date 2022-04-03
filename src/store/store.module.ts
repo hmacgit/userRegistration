ec5dafd53a24123ee6be2365dc5a83aa060301cd
Mac <hung.mac@dbschenker.com>
2022-04-03 02:23:57 -0400

    nativation works

--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -4,13 +4,16 @@ import { NgxsModule } from '@ngxs/store';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { DEVTOOLS_REDUX_CONFIG, LOGGER_CONFIG, OPTIONS_CONFIG, STATES_MODULES } from './store.config';
+import {NgxsRouterPluginModule} from '@ngxs/router-plugin';
 
 @NgModule({
   imports: [
     CommonModule,
     NgxsModule.forRoot(STATES_MODULES, OPTIONS_CONFIG),
     NgxsReduxDevtoolsPluginModule.forRoot(DEVTOOLS_REDUX_CONFIG),
-    NgxsLoggerPluginModule.forRoot(LOGGER_CONFIG)
+    NgxsLoggerPluginModule.forRoot(LOGGER_CONFIG),
+    //NgxsModule.forRoot([]),
+    NgxsRouterPluginModule.forRoot()
   ],
   exports: [NgxsModule]
 })
